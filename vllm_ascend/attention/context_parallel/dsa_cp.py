30699e7d5cc5af74aa824c196d57e439fa4c66a3
zhangtao0408 <365968531@qq.com>
2026-07-17 10:35:46 +0800

    [Performance][DSA-CP] Cache local token metadata
    Signed-off-by: zhangtao0408 <365968531@qq.com>

--- vllm_ascend/attention/context_parallel/dsa_cp.py
+++ vllm_ascend/attention/context_parallel/dsa_cp.py
@@ -384,6 +384,7 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
         **kwargs,
     ) -> AscendDSAMetadata:
         assert self.compressor_ratio <= 1, "vLLM-Ascend only support SWA-layer for Deepseek-V4 now."
+        self.common_ratio_to_sas_metadata = kwargs.get("common_ratio_to_sas_metadata", {})
         num_reqs = common_attn_metadata.num_reqs
         num_input_tokens = common_attn_metadata.num_input_tokens
         assert num_reqs <= self.max_num_draft_reqs, (
@@ -494,6 +495,7 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
             use_cache=False,
             local_query_start_loc=self.spec_local_query_start_loc[draft_index - 1],
             local_seq_lens=self.spec_local_seq_lens[draft_index - 1],
+            draft_index=draft_index,
         )
         _, _, _, _, local_query_start_loc_cpu, local_seq_lens_cpu, _, _ = self._build_local_token_metadata(
             num_reqs=num_reqs,
@@ -502,6 +504,7 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
             query_start_loc=query_start_loc_cpu,
             seq_lens=self.seq_lens_cpu[:num_reqs],
             use_cache=False,
+            draft_index=draft_index,
         )
         local_seq_lens_q_cpu = local_query_start_loc_cpu[1 : num_reqs + 1] - local_query_start_loc_cpu[:num_reqs]
         max_local_query_len = max(1, int(local_seq_lens_q_cpu.max().item()))
@@ -741,6 +744,7 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
         use_cache,
         local_query_start_loc=None,
         local_seq_lens=None,
+        draft_index: int | None = None,
     ):
         """
         For example:
@@ -766,6 +770,28 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
         tokens_per_rank = num_tokens_pad // tp_size
         local_start = tp_rank * tokens_per_rank
         local_end = local_start + tokens_per_rank
+
+        device_kind = "cpu" if input_positions is None else "device"
+        cache_key = f"cp_local_token_metadata_{device_kind}_{draft_index}"
+        cached_metadata = self.common_ratio_to_sas_metadata.get(cache_key)
+        if cached_metadata is not None:
+            cached_query_start_loc, cached_seq_lens, local_cos, local_sin = cached_metadata
+            if local_query_start_loc is not None:
+                local_query_start_loc[1 : num_reqs + 1].copy_(cached_query_start_loc[1:])
+                local_seq_lens[:num_reqs].copy_(cached_seq_lens)
+            else:
+                local_query_start_loc = cached_query_start_loc
+                local_seq_lens = cached_seq_lens
+            return (
+                local_start,
+                local_end,
+                tokens_per_rank,
+                num_tokens_pad,
+                local_query_start_loc[: num_reqs + 1],
+                local_seq_lens[:num_reqs],
+                local_cos,
+                local_sin,
+            )
 
         if local_query_start_loc is not None:
             local_query_start_loc.fill_(0)
@@ -807,7 +833,7 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
         else:
             local_cos = None
             local_sin = None
-        return (
+        result = (
             local_start,
             local_end,
             tokens_per_rank,
@@ -817,6 +843,8 @@ class AscendDSACPMetadataBuilder(AttentionMetadataBuilder[AscendDSAMetadata]):
             local_cos,
             local_sin,
         )
+        self.common_ratio_to_sas_metadata[cache_key] = result[4:]
+        return result
 
     def _get_cmp_seqlens_for_metadata(self, has_prefill):
         if self.compressor_ratio <= 1:
